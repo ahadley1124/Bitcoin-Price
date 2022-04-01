5bd9e4692a6607f3098b98fa3dbf011d197f31e7
ahadley1124 <ahadley1124@gmail.com>
2022-04-01 00:04:54 -0400

    Rewrote the entire JS source code
    JS code now prints the price difference to the console.  The first price is set to the first price the the server sends. This price could also be the average price of the bitcoin you have bought.

--- JS_Implimentation/main.js
+++ JS_Implimentation/main.js
@@ -1,71 +1,33 @@
-const Web = require('websocket').w3cwebsocket;
-const ws = new Web('wss://ws-feed.exchange.coinbase.com');
+const WebSocket = require('websocket').w3cwebsocket;
+var ws = new WebSocket('wss://ws-feed.exchange.coinbase.com');
 var firstPrice = 0;
-const recipients = 'austinhadley2004@gmail.com';
-
-ws.onerror = function(e) {
-    console.log('Error: ' + e.data);
-}
-
-ws.onclose = function() {
-    console.log('Reconnecting...');
-    ws = new Web('wss://ws-feed.exchange.coinbase.com');
-}
-
 ws.onopen = function() {
     ws.send(JSON.stringify({
-        "type": "subscribe",
-        "product_ids": ["BTC-USD"],
-        "channels": ["ticker"]
+        type: 'subscribe',
+        product_ids: ['BTC-USD'],
+        channels: ['ticker']
     }));
-}
-
+    console.log('connected. Time is: ' + new Date());
+};
 ws.onmessage = function(e) {
     var data = JSON.parse(e.data);
-    if(firstPrice == 0) {
-        firstPrice = data.price;
-    }
-    else {
-        // when the price changes by more than .25%, change the firstPrice variable
-        checkPrice(data.price, .25, recipients);
-    }
-}
-
-// send an email to a list of recipients
-function sendEmail(recipients, subject, body) {
-    var nodemailer = require('nodemailer');
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'ahadley1124@gmail.com',
-            pass: 'wzkbhmjuzssnyihx'
+    if (data.type == 'ticker') {
+        var price = data.price;
+        if (firstPrice == 0) {
+            firstPrice = price;
         }
-    });
+        console.log((price - firstPrice).toFixed(2));
+    }
+};
 
-    var mailOptions = {
-        from: 'ahadley1124@gmail.com',
-        to: recipients,
-        subject: subject,
-        text: body
+ws.onclose = function() {
+    console.log('reconnecting...');
+    ws = new WebSocket('wss://ws-feed.exchange.coinbase.com');
+    ws.onopen = function() {
+        ws.send(JSON.stringify({
+            type: 'subscribe',
+            product_ids: ['BTC-USD'],
+            channels: ['ticker']
+        }));
     };
-
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-}
-
-// send an email if the price changes by a certain percentage
-function checkPrice(price, percent, recipients) {
-    var change = (price - firstPrice) / firstPrice;
-    if(change > percent) {
-        sendEmail(recipients, 'Price Change', 'The price has changed by ' + change + '%');
-        firstPrice = price;
-    }
-}
-
-// start the websocket connection
-ws.onopen();
+};
--- python/test.py
+++ python/test.py
@@ -1,35 +1,20 @@
-import websockets
-import asyncio
+#create websocket connection
+import websocket
 import json
 
-#create a connection to the coinbase websocket
-async def connect_to_websocket():
-    async with websockets.connect('wss://ws-feed.exchange.coinbase.com') as websocket:
-        await websocket.send(json.dumps({
-            "type": "subscribe",
-            "product_ids": ["BTC-USD"],
-            "channels": ["ticker"]
-        }))
-        while True:
-            message = await websocket.recv()
-            print(message["price"])
+#connect to coinbase websocket
+ws = websocket.create_connection("wss://ws-feed.exchange.coinbase.com")
 
-#send an email when the price of bitcoin changes by more than 100 dollars
-async def send_email(price, current_time):
-    import smtplib, ssl
-    port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
-    sender_email = "ahadley1124@gmail.com"  # Enter your address
-    receiver_email = "austinhadley2004@gmail.com"  # Enter receiver address
-    password = 'wzkbhmjuzssnyihx'
-    message = """\
-    Subject: Bitcoin Update:\n\n\
-    At: {current_time}, The price of bitcoin is: {price}""".format(current_time=current_time, price=price)
-    #send the email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+#subscribe to the BTC-USD ticker
+ws.send(json.dumps({
+    'type' : 'subscribe',
+    'product_ids' : ['BTC-USD'],
+    'channels' : ['ticker']
+}))
 
-#run the program
-asyncio.get_event_loop().run_until_complete(connect_to_websocket())
+#constantly listen for messages and print the price from the json message
+while True:
+    message = [json.loads(ws.recv())]
+    message_length = len(message)
+    for i in range(message_length):
+        print(message[4]['price'])
